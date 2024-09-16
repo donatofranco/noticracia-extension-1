c762ab9962124805c7517568b5aa4398bd7d4094
donatofranco <francoedonato@gmail.com>
2024-09-16 15:54:03 -0300

    Initial commit

---
+++ src/main/java/noticracia/ClarinInformationSource.java
@@ -0,0 +1,10 @@
+package noticracia;
+
+import noticracia.entities.InformationSource;
+
+public class ClarinInformationSource extends InformationSource {
+    @Override
+    public void startInformationCollection(String s) {
+
+    }
+}
