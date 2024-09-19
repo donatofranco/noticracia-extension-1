ed00dbf98fa2672db5ca1491034e8c44d9e7359d
donatofranco <francoedonato@gmail.com>
2024-09-18 21:15:27 -0300

    Update to 1.1.0

--- src/main/java/noticracia/ClarinInformationSource.java
+++ src/main/java/noticracia/ClarinInformationSource.java
@@ -1,10 +1,40 @@
 package noticracia;
 
 import noticracia.entities.InformationSource;
+import noticracia.services.information.broker.InformationSourceBroker;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class ClarinInformationSource extends InformationSource {
-    @Override
-    public void startInformationCollection(String s) {
+    public ClarinInformationSource(InformationSourceBroker informationSourceBroker) {
+        super(informationSourceBroker);
+    }
 
+    @Override
+    public boolean startProcess(String s) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(10000); // Pausa por 5 segundos
+                this.refresh(Map.of(
+                        "link",
+                        "textito textito textito textito textito textito textito textito"
+                ));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("El hilo fue interrumpido");
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public void stopProcess() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
     }
 }
