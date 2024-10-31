5d109efffe6fefa91ef3ab277b32383c974aa2b3
Juan Manuel Losada <juanmalosada01@gmail.com>
2024-10-30 21:04:29 -0300

    Actualizo versión para la iteración 0

--- src/main/java/noticracia/ClarinInformationSource.java
+++ src/main/java/noticracia/ClarinInformationSource.java
@@ -3,17 +3,34 @@ package noticracia;
 import noticracia.entities.InformationSource;
 import services.InformationGenerator;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ClarinInformationSource implements InformationSource {
 
     @Override
     public Map<String, String> getInformation(String searchCriteria) {
-        return InformationGenerator.getInformation(searchCriteria);
+        String userHome = System.getProperty("user.home");
+        Path path = Paths.get(userHome, searchCriteria + ".txt");
+        String content;
+        try {
+            content = Files.readString(path);
+        }catch (IOException e){
+            return Map.of("NO ENCONTRE","NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA");
+        }
+        return Arrays.stream(content.split("<>"))
+                .map(pair -> pair.split("==="))
+                .collect(Collectors.toMap(parts -> parts[0].trim(), parts -> parts[1].trim()));
     }
 
     @Override
     public String getName() {
         return "Clarinete";
     }
+
 }
--- src/main/java/services/InformationGenerator.java
+++ src/main/java/services/InformationGenerator.java
@@ -10,17 +10,5 @@ import java.util.stream.Collectors;
 
 public class InformationGenerator {
 
-    public static Map<String, String> getInformation(String searchCriteria){
-        String userHome = System.getProperty("user.home");
-        Path path = Paths.get(userHome, searchCriteria + ".txt");
-        String content;
-        try {
-            content = Files.readString(path);
-        }catch (IOException e){
-            return Map.of("NO ENCONTRE","NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA");
-        }
-        return Arrays.stream(content.split("<>"))
-                .map(pair -> pair.split("==="))
-                .collect(Collectors.toMap(parts -> parts[0].trim(), parts -> parts[1].trim()));
-    }
+
 }
