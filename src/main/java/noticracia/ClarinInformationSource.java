89e83bf7fa9da40a868a9e5a8294c3dcad5a245c
Juan Manuel Losada <juanmalosada01@gmail.com>
2024-11-07 22:15:11 -0300

    Quito link de la extension

--- src/main/java/noticracia/ClarinInformationSource.java
+++ src/main/java/noticracia/ClarinInformationSource.java
@@ -9,23 +9,24 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ClarinInformationSource implements InformationSource {
 
     @Override
-    public Map<String, String> getInformation(String searchCriteria) {
+    public Set<String> getInformation(String searchCriteria) {
         String userHome = System.getProperty("user.home");
         Path path = Paths.get(userHome, searchCriteria + ".txt");
         String content;
         try {
             content = Files.readString(path);
-        }catch (IOException e){
-            return Map.of("NO ENCONTRE","NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA");
+        } catch (IOException e) {
+            return Set.of("NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA NADA");
         }
         return Arrays.stream(content.split("<>"))
-                .map(pair -> pair.split("==="))
-                .collect(Collectors.toMap(parts -> parts[0].trim(), parts -> parts[1].trim()));
+                .map(String::trim)
+                .collect(Collectors.toSet());
     }
 
     @Override
